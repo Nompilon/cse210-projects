196eaf408dd693304df4037e5d55b10326db06a8
Nompilon <ngwenyanompilo@gmail.com>
2025-03-10 22:35:36 +0200

    added a comment

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,7 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        //Ask the user for their name.
+        //Ask the user for their name
         Console.WriteLine("What is your first name? ");
         String first = Console.ReadLine();
 
