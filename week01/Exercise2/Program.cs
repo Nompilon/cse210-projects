cebb3878daa12cabb06dd92dafd14bd21210d7f0
Nompilon <ngwenyanompilo@gmail.com>
2025-03-10 22:22:56 +0200

    finished program

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -1,9 +1,46 @@
 using System;
+using System.Transactions;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.WriteLine("What is your grade percentage? ");
+        String answer = Console.ReadLine();
+        int grade = int.Parse(answer);
+
+        string letter = "";
+        string comment = "";
+
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade >= 80)
+        {
+            letter = "B";
+        }
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }
+        else if (grade >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+        Console.WriteLine($"Your grade is {letter}");
+        if (grade >= 70)
+        {
+            comment = "Congradulations, you have passed!";
+        }
+        else
+        {
+            comment = "Your mark is below the bar. You can try again next term!";
+        }
+        Console.WriteLine($"{comment}");
     }
 }
