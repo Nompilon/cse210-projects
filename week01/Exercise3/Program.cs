911d5b9eed26a4e2bc8012886370b54867c991f0
Nompilon <ngwenyanompilo@gmail.com>
2025-03-11 20:05:02 +0200

    program completed

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 15);
+        int guess = 0;
+
+        while (guess != number)
+        {
+            Console.WriteLine("What is the magic number? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (guess > number)
+            {
+                Console.WriteLine("Lower");
+            }
+
+            else if (guess < number)
+            {
+                Console.WriteLine("Higher");
+            }
+
+            else
+            {
+                Console.WriteLine("Wow, you have guessed the magic number!");
+            }
+        }
+
     }
+
+
+
 }
