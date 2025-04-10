39dd6a1850273e90540a5e7617a009a25d226938
Nompilon <ngwenyanompilo@gmail.com>
2025-04-10 06:29:50 +0200

    program fixed now printing

--- week03/ScriptureMemorizer/Program.cs
+++ week03/ScriptureMemorizer/Program.cs
@@ -8,11 +8,12 @@ class Program
         Reference reference = new Reference("John", 3, 16);
         Scripture scripture = new Scripture(reference,"For God so loved the world that he gave his one and only Son that whoever believes in him shall not perish but have eternal life.");
 
-        Console.Clear();
-        scripture.GetDisplayText();
-
+        
         while (true)
         {
+            Console.Clear();
+            Console.WriteLine(scripture.GetDisplayText());
+
             Console.WriteLine("Press Enter to hide a word or type 'quit' to exit:");
             string userInput = Console.ReadLine();
 
