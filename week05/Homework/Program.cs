870e15485a3a50a9619c8e9aafc8b75b69b35a57
Nompilon <ngwenyanompilo@gmail.com>
2025-04-12 10:35:13 +0200

    Completed

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -1,9 +1,20 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        Assignment assignment1 = new Assignment("Okuhle", "Graphs");
+        Console.WriteLine(assignment1.GetSummary());
+
+        MathAssignment assignment2 = new MathAssignment("Okuhle", "Quadratics", " 3.4", "1 - 9");
+        Console.WriteLine(assignment2.GetSummary());
+        Console.WriteLine(assignment2.GetHomeworklist());
+
+        WritingAssignment assignment3 = new WritingAssignment("Ruth", "Essay writing", "The day I will nenver forget"); ;
+        Console.WriteLine(assignment3.GetSummary());
+        Console.WriteLine(assignment3.GetWritingInformation());
     }
 }
+
